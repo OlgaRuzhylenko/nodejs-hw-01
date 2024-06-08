22841453dab4d45c5025476829571b298275a774
Olga <olyaruzhylenko@gmail.com>
2024-06-09 00:43:01 +0300

    addOneContact

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 //сюди імпортуємо всі функції
 import generateContacts from './scripts/generateContacts';
-import getAllContacts from './scripts/getAllContacts'
+import getAllContacts from './scripts/getAllContacts';
+import addOneContact from './scripts/addOneContact.js'
 
-const invokeAction = async ({ action, number }) => {
+const invokeAction = async ({ action, number, ...data }) => {
   switch (action) {
     case 'generate':
       const allGeneratedContacts = await generateContacts(number);
       return console.log(allGeneratedContacts);
     case 'list':
-        const allContacts = await getAllContacts();
-        return console.log(allContacts);
-        case 'add':
-            
+      const allContacts = await getAllContacts();
+      return console.log(allContacts);
+    case 'add':
+const newContact = await addOneContact(data);
+return console.log(newContact);
   }
 };
 invokeAction({ action: 'generate' });
 invokeAction({ action: 'list' });
-invokeAction({ action: 'add', });
+invokeAction({ action: 'add' });
--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,7 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import {getAllContacts} from './getAllContacts.js';
+import fs from 'fs/promises';
 
 export const addOneContact = async () => {
-
+  const contacts = await getAllContacts();
+  const newContact = createFakeContact();
+  contacts.push(newContact);
+  await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+  return newContact;
 };
 
 await addOneContact();
